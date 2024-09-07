f92b7f79b67ee2041d99184aa4f19a887cf832c0
basednai <naiym.harris@icloud.com>
2024-09-06 22:06:03 -0400

    finished project

---
+++ script.js
@@ -0,0 +1,10 @@
+let content = document.querySelectorAll(".cardContent")
+content.forEach((card) => {
+    card.textContent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. \
+    Hic eveniet atque autem eum quia excepturi."
+})
+
+let contentHeadings = document.querySelectorAll(".cardHeading")
+contentHeadings.forEach((card) => {
+    card.textContent = "Lorem Ipsum."
+})
